73165ebaa5511cc1030cfca54836a05efddcf945
Christian Ramelow <info@codekandis.net>
2017-06-15 20:31:56 +0200

    Initial commit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php declare( strict_types = 1 );
+namespace MaMei\MaMeiCom\Tests
+{
+	use function dirname;
+
+	/**
+	 * Represents the bootstrap script of the unit tests.
+	 * @package codekandis\pharty
+	 * @author  Christian Ramelow <info@codekandis.net>
+	 */
+	require_once dirname( __DIR__, 1 ) . '/vendor/autoload.php';
+}
