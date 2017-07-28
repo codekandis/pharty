5431f7b8693107c6fd4accf73cb1b05bcc3e3522
Christian Ramelow <info@codekandis.net>
2017-07-28 09:33:05 +0200

    Introduces the method `EnvironmentInterface::getHttpData()`.

--- src/Mvc/EnvironmentInterface.php
+++ src/Mvc/EnvironmentInterface.php
@@ -1,6 +1,8 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\Pharty\Mvc;
 
+use CodeKandis\Pharty\Http\HttpDataInterface;
+
 /**
  * Represents the interface of all application environment classes.
  * @package codekandis/pharty
@@ -13,4 +15,10 @@ interface EnvironmentInterface
 	 * @return string The name of the application.
 	 */
 	public function getApplicationName(): string;
+
+	/**
+	 * Gets the HTTP data of the request.
+	 * @return HttpDataInterface The HTTP data of the request.
+	 */
+	public function getHttpData(): HttpDataInterface;
 }
