a4199623c7f235b2794f288d0983d66ac3f842d9
Christian Ramelow <info@codekandis.net>
2018-02-26 13:22:30 +0100

    Adds the exception `ArrayKeyNotFoundException`.

---
+++ src/Data/ArrayKeyNotFoundException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Pharty\Data;
+
+use CodeKandis\Pharty\Base\LogicException;
+
+/**
+ * Represents an exception if an array key does not exist.
+ * @package codekandis/pharty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class ArrayKeyNotFoundException extends LogicException
+{
+}
