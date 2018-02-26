1b9c5b6e24f74e63534df16b6a280712bed5c6f7
Christian Ramelow <info@codekandis.net>
2018-02-26 14:10:19 +0100

    Adds the exception `FileSystemEntryNotFoundException`.

---
+++ src/Io/FileSystemEntryNotFoundException.php
@@ -0,0 +1,11 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Pharty\Io;
+
+/**
+ * Represents the exception if a files system entry does not exist.
+ * @package codekandis/pharty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class FileSystemEntryNotFoundException extends IoException
+{
+}
