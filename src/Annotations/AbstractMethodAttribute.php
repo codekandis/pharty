e1706ca1890f182d46de16018033c21fd46bba72
Christian Ramelow <info@codekandis.net>
2018-03-08 12:13:35 +0100

    Adds the class `AbstractMethodAttribute`.

---
+++ src/Annotations/AbstractMethodAttribute.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Pharty\Annotations;
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * Represents base class of all class attributes.
+ * @package codekandis/pharty
+ * @Annotation
+ * @Target("METHOD")
+ */
+abstract class AbstractMethodAttribute
+{
+}
