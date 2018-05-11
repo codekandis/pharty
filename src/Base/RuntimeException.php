6be7401f2cfbe87d8b548664cf614000452816bb
Christian Ramelow <info@codekandis.net>
2018-05-11 17:21:50 +0200

    Adds the exception `RuntimeException`.

---
+++ src/Base/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\Pharty\Base;
+
+use RuntimeException as SplRuntimeException;
+
+/**
+ * Represents the base exception of all runtime exceptions.
+ * @package codekandis/pharty
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class RuntimeException extends SplRuntimeException
+{
+}
