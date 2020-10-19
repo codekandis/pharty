003584b931b8f0932eece9b020754ae3daf434a8
Christian Ramelow <info@codekandis.net>
2020-10-19 22:13:35 +0200

    Updates the interface `ListInterface`.

--- src/Collections/ListInterface.php
+++ src/Collections/ListInterface.php
@@ -27,6 +27,23 @@ interface ListInterface extends ImmutableListInterface
 	public function insert( int $index, $element ): void;
 
 	/**
+	 * Replaces an element with a new element.
+	 * @param mixed $element The element to replace.
+	 * @param mixed $replacement The new element.
+	 * @throws ElementNotFoundException The element does not exist.
+	 */
+	public function replace( $element, $replacement ): void;
+
+	/**
+	 * Replaces an element at a specified index with a new element.
+	 * @param int $index The index of the element to replace.
+	 * @param mixed $replacement The new element.
+	 * @throws OutOfBoundsException The specified index is out of bounds.
+	 * @throws ElementNotFoundException The element at the index does not exist.
+	 */
+	public function replaceAt( int $index, $replacement ): void;
+
+	/**
 	 * Removes an element from the list.
 	 * @param mixed $element The element to remove.
 	 * @throws ElementNotFoundException The element does not exist.
